e5cb12196640a0760f51c66ca135151fac2de7a2
vkazhdan <vkazhdan@anotheria.net>
2018-11-07 17:35:20 +0200

    Added hibernate communication test Fixed hibernate dependencies

--- src/main/java/com/javacodegeeks/configuration/HibernateConfiguration.java
+++ src/main/java/com/javacodegeeks/configuration/HibernateConfiguration.java
@@ -41,6 +41,8 @@ public class HibernateConfiguration {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
         dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
         return dataSource;
     }
      
---
+++ src/main/java/com/javacodegeeks/service/EmployeeServiceTest.java
@@ -0,0 +1,25 @@
+package com.javacodegeeks.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmployeeServiceTest {
+
+	@Autowired
+	private EmployeeService employeeService;
+
+	public void run() {
+		System.out.println("Started");
+		System.out.println("EmployeeService: " + employeeService);
+		System.out.println("Employees: " + employeeService.findAllEmployees());
+	}
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext("com.javacodegeeks");
+		EmployeeServiceTest test = applicationContext.getBean(EmployeeServiceTest.class);
+		test.run();
+	}
+}
