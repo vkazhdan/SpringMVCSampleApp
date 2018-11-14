ef2eaf1fed4a6717b58699d65693f036c7bb8107
vkazhdan <vkazhdan@anotheria.net>
2018-11-14 19:30:04 +0200

    Migrated to h2 database

--- src/main/java/com/javacodegeeks/model/Employee.java
+++ src/main/java/com/javacodegeeks/model/Employee.java
@@ -1,21 +1,12 @@
 package com.javacodegeeks.model;
 
-import java.math.BigDecimal;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
-import org.hibernate.annotations.Type;
-import org.hibernate.validator.constraints.NotEmpty;
-import org.joda.time.LocalDate;
-import org.springframework.format.annotation.DateTimeFormat;
 
 
 @Entity
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO EMPLOYEE (id, name) VALUES (1, 'Ivan');
+INSERT INTO EMPLOYEE (id, name) VALUES (1, 'Maria');
